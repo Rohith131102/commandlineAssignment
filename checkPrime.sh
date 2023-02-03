4fe453f660db51f37ff2c680eb10f135c89bd8ff
Rohith131102 <123619674+Rohith131102@users.noreply.github.com>
2023-02-03 12:34:23 +0530

    Add files via upload
    checks whether the entered argument is prime or not

---
+++ checkPrime.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Function to check if it is prime or not
+function isPrime() {
+  num=$1 # Intilizing num by the first argument
+  if [ $num -lt 0 ]; then  #error generated when user inputs negative number
+    echo "error,$num is negative number,please enter only positive number."
+    return
+  elif [[ $num -eq 1 ]]; then  #special case
+    echo "1 is not a prime number"
+    return 
+  fi
+	 
+  for (( i=2; i<$((num/2+1)); i++ )) # Loop from 2 to num/2 to verify its divisors
+  do
+    if [ $(($num%$i)) -eq 0 ]; then # if found it is not prime number
+      echo "$num is not a prime number."
+      return
+    fi
+  done
+  echo "$num is a prime number." # else it is prime
+}
+# Reading input from user and storing in num
+read -p "Enter a number: " num
+isPrime $num # Passing it to function isPrime
+
+exit 0
